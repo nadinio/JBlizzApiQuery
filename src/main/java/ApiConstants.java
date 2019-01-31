096f2c772503f92bde995ee2eabb707dfefdb44a
N. Dinio <nika1667@gmail.com>
2019-01-31 10:00:41 -0500

    Basic test

--- src/main/java/ApiConstants.java
+++ src/main/java/ApiConstants.java
@@ -2,5 +2,5 @@ public class ApiConstants {
 
     public static final String BASE_REST_URL = "https://us.api.blizzard.com/";
 
-    public static final String ACCESS_TOKEN = "USNr7oQ7H5oqr7WfMMrKG7ROyygJ6axMMg";
+    public static final String ACCESS_TOKEN = "USf1HFw0pptpMI55G0V8KMf4f6Ucbh7gqk";
 }
